319b3fee6989bda1ef2ed59e610fc7479286dc2c
Tyler Long <tyler4long@gmail.com>
2018-05-03 15:02:05 +0800

    send-sms.php

---
+++ send-sms.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require('vendor/autoload.php');
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+$rcsdk = new RingCentral\SDK\SDK(getenv('RINGCENTRAL_CLIENT_ID'), getenv('RINGCENTRAL_CLIENT_SECRET'), getenv('RINGCENTRAL_SERVER_URL'));
+
+$platform = $rcsdk->platform();
+
+$platform->login(getenv('RINGCENTRAL_USERNAME'), getenv('RINGCENTRAL_EXTENSION'), getenv('RINGCENTRAL_PASSWORD'));
+
+$apiResponse = $platform->post('/account/~/extension/~/sms', array(
+    'from' => array('phoneNumber' => getenv('RINGCENTRAL_USERNAME')),
+    'to' => array(
+        array('phoneNumber' => getenv('RINGCENTRAL_RECEIVER')),
+    ),
+    'text' => 'Message content',
+));
+
+print_r($apiResponse->json()->id);
