e883126be4b91af2647b40b9577de82ccd6a77e8
Tyler Long <tyler4long@gmail.com>
2018-05-03 15:25:29 +0800

    track-status.php

---
+++ track-status.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require('vendor/autoload.php');
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+$rcsdk = new RingCentral\SDK\SDK(getenv('RINGCENTRAL_CLIENT_ID'), getenv('RINGCENTRAL_CLIENT_SECRET'), getenv('RINGCENTRAL_SERVER_URL'));
+
+$platform = $rcsdk->platform();
+
+$platform->login(getenv('RINGCENTRAL_USERNAME'), getenv('RINGCENTRAL_EXTENSION'), getenv('RINGCENTRAL_PASSWORD'));
+
+$r = $platform->post('/account/~/extension/~/sms', array(
+    'from' => array('phoneNumber' => getenv('RINGCENTRAL_USERNAME')),
+    'to' => array(
+        array('phoneNumber' => getenv('RINGCENTRAL_RECEIVER')),
+    ),
+    'text' => 'Message content',
+));
+
+$messageId = $r->json()->id;
+$r = $platform->get("/account/~/extension/~/message-store/{$messageId}");
+print_r($r->json()->messageStatus);
